a3a837d8b9423238e40de801d42db72c13efc421
Hurley Swan <antontit89@gmail.com>
2020-03-15 20:40:28 +0700

    post create,update,delete from public

--- blog/urls.py
+++ blog/urls.py
@@ -3,14 +3,17 @@ from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.flatpages import views
-from posts.views import index, blog, post, search
+from posts.views import index, blog, post, search, post_create, post_update, post_delete
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index),
     path('blog/', blog, name='post-list'),
     path('search/', search, name='search'),
+    path('create/', post_create, name='post-create'),
     path('post/<id>/', post, name='post-detail'),
+    path('post/<id>/update/', post_update, name='post-update'),
+    path('post/<id>/delete/', post_delete, name='post-delete'),
     path('tinymce/', include('tinymce.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
     # path('<path:url>', views.flatpage),
